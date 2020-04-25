74ebc6f2ed36aafaf421f4b8f43a9513059c709b
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-04-24 22:19:48 -0700

    register view and validation set up, bcrypt password hash test

--- to_do_list/apps/list_app/urls.py
+++ to_do_list/apps/list_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path("signup", views.home),
+    path("register", views.register),
 ]
--- to_do_list/apps/list_app/views.py
+++ to_do_list/apps/list_app/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import *
+from django.contrib import messages
 
 def index(request):
     return render(request, "list_app/index.html")
 
 def home(request):
-    return render(request, "list_app/home.html")
-
-def signup(request):
     pass
+
+def register(request):
+    errors = User.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/")
+    else:
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        password = bcrypt.hashpw(request.POST["password"].encode(), bcrypt.gensalt())
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password
+        )
+        request.session["user_id"] = user.id
+        messages.success(request, "Successfully registered!")
+        return redirect("/user")
